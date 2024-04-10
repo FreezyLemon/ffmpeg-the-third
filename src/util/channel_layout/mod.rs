ea4026e1af730048ed24bf1f4b9743162510ff2b
FreezyLemon <contact@freezylemon.com>
2024-04-10 20:06:10 +0200

    More cfg flags

--- src/util/channel_layout/mod.rs
+++ src/util/channel_layout/mod.rs
@@ -1,11 +1,17 @@
-pub mod channel;
-pub mod iter;
 pub mod mask;
 pub mod order;
 
-pub use channel::Channel;
 pub use mask::ChannelLayout;
 pub use order::ChannelOrder;
+
+#[cfg(feature = "ffmpeg_5_1")]
+pub mod channel;
+#[cfg(feature = "ffmpeg_5_1")]
+pub use channel::Channel;
+
+// TODO:
+#[cfg(feature = "ffmpeg_5_1")]
+pub mod iter;
 
 #[cfg(feature = "ffmpeg_5_1")]
 pub mod layout;
