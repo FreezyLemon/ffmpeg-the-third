edc84a0a3ff0ffcf391ec9692c3bc0e3c9db591e
FreezyLemon <contact@freezylemon.com>
2026-03-03 21:44:00 +0100

    assume size_of and align_of are in prelude

--- src/codec/picture.rs
+++ src/codec/picture.rs
@@ -1,5 +1,4 @@
 use std::marker::PhantomData;
-use std::mem;
 use std::slice;
 
 use crate::ffi::*;
@@ -58,7 +57,7 @@ impl<'a> Picture<'a> {
 
     pub fn new(format: format::Pixel, width: u32, height: u32) -> Result<Self, Error> {
         unsafe {
-            let ptr = av_malloc(mem::size_of::<AVPicture>() as size_t) as *mut AVPicture;
+            let ptr = av_malloc(size_of::<AVPicture>() as size_t) as *mut AVPicture;
 
             match avpicture_alloc(ptr, format.into(), width as c_int, height as c_int) {
                 0 => Ok(Picture {
